1f808d931afcb76903cf4aee17dee3abfbcd9f4b
jmcarrb <jmcarrb@gmail.com>
2014-04-26 14:11:03 +0100

    Update get_history.php

--- hackz/get_history.php
+++ hackz/get_history.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL);
 try {
-$location = "C:/Users/Joshua/AppData/Local/Google/Chrome/User Data/Default/History";
+$location = "C:/Users/Joshua/AppDatasLOL/Local/Google/Chrome/User Data/Default/History";
 $database = new PDO("sqlite:" . $location . "");
-
+$haha = false;
 $query = "SELECT * FROM urls";
 
 $statement = $database->prepare($query);
